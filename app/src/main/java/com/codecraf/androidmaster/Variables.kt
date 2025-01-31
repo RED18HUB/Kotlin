b6579c21b67dfe697f96a3a4b8e0f77c4c391049
red19hub <mialexrojas650@gmail.com>
2025-01-31 08:53:42 -0500

    inicial

---
+++ app/src/main/java/com/codecraf/androidmaster/Variables.kt
@@ -0,0 +1,48 @@
+package com.codecraf.androidmaster
+var age:Int=30
+var age2:Int=56
+val example:Long=546885
+fun main(){
+    varibalesNumericas()
+
+    add(15,10)
+    showMyName("jonas")
+    val mySubstract = substract(10,5)
+    println(mySubstract)
+    // poner la f para decir que es de tipo float
+    val floatDecimal:Float=30.1f
+    val doubleNumero:Double=55.97
+   //Char
+    val charExample1:Char='q'
+    var charExample2:Char='2'
+    val charExample4:Char='@'
+    //STRING
+    val stringExample:String="1"
+    val stringExample2:String="2"
+    val stringExample3:String="3"
+    //Boolean
+    val booleanExample:Boolean=true
+    val booleanExample2:Boolean=false
+    var stringConcatenada:String="HOLA"
+
+}
+fun varibalesNumericas(){
+
+    println(age)
+}
+fun showMyAge(){
+    println("mi edad es ")
+}
+fun add(firstNumber:Int, seconNumber:Int){
+    println(firstNumber + seconNumber)
+
+}
+
+fun showMyName(name:String){
+    println("Mi nombre es $name")
+}
+fun substract(firstNUmber:Int, secondNUmber:Int):Int{
+    return firstNUmber-secondNUmber
+
+
+}
